4fe570717fae107e1aae5dd121c0481f94e2445e
madhur17 <madhurag1702@gmail.com>
2024-10-19 18:17:32 +0530

    Bug Fix

--- Backened/db/connect.ts
+++ Backened/db/connect.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-
+dotenv.config()
 const MongoUrl:string|undefined=process.env.MONGO_DB_URL
 
 const connect=async()=>{
+    
     try {
         if (!MongoUrl) {
             throw new Error("MongoDB URL is not defined in environment variables.");
